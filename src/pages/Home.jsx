97a2a7511947bf8953f705b68455501cd0e72a94
KipkuruiYegon <kevinkipkurui1@gmail.com>
2025-02-02 22:37:11 +0300

    setting the State Change for Search Query input form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
     const movies = [
         {id:1, title:"John Wick", release_date: "2020"},
         {id:2, title:"Terminator", release_date: "1999"},
@@ -13,7 +15,12 @@ function Home() {
 
     return (<div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search for Movie....." className="search-input" />
+            <input type="text"
+            placeholder="Search for Movie....."
+            className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            />
             <button type="submit"  className="search-button" >Search</button>
         </form>
         
