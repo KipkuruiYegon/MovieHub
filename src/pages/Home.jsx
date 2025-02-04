60f4f527b0ab75f02296561192c5c7be9e8d64cb
KipkuruiYegon <kevinkipkurui1@gmail.com>
2025-02-04 18:17:59 +0300

    setting use effect functionality, render movies from the api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,29 @@
 import MovieCard from "../components/MovieCard"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../css/Home.css";
+import { searchMovies, getPopularMovies } from "../services/api";
 
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
-    const movies = [
-        {id:1, title:"John Wick", release_date: "2020"},
-        {id:2, title:"Terminator", release_date: "1999"},
-        {id:3, title:"Matrix", release_date: "1998"},
-        {id:4, title:"Mission Impossible", release_date: "1991"}
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    ];
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Failed to Load Movies.....")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        loadPopularMovies()
+    }, [])
 
     const handleSearch = (e) => {
         e.preventDefault()
